c39f2044f77f06915d01681b79d48c378d813bff
Dorian-hasg <dorianfernando9994@gmail.com>
2024-10-19 11:50:47 -0400

    Add files via upload

---
+++ Depurador de codigo/ConsoleApp19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Pedir al usuario el primer número
+        Console.WriteLine("Introduce el primer número:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        // Pedir al usuario el segundo número
+        Console.WriteLine("Introduce el segundo número:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        // Sumar los dos números
+        double suma = num1 + num2;
+
+        // Mostrar el resultado
+        Console.WriteLine("La suma de los dos números es: " + suma);
+    }
+}
