4f61be218da31b24f9407cf08653c19b1fab30cf
Dorian-hasg <dorianfernando9994@gmail.com>
2024-10-19 11:59:42 -0400

    Add files via upload

---
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Galaxias cercanas a la Vía Láctea!");
+
+        // Creamos una lista de objetos Galaxia
+        List<Galaxia> galaxias = new List<Galaxia>
+        {
+            new Galaxia { Nombre = "Cartwheel", Tipo = TipoGalaxia.Lenticular },
+            new Galaxia { Nombre = "Andromeda", Tipo = TipoGalaxia.Espiral },
+            new Galaxia { Nombre = "Waffei 1", Tipo = TipoGalaxia.Elíptica }
+        };
+
+        // Iteramos sobre la lista y mostramos los datos de cada galaxia
+        foreach (var galaxia in galaxias)
+        {
+            Console.WriteLine($"{galaxia.Nombre}, {galaxia.Tipo}");
+        }
+    }
+}
+
+// Definimos la enumeración TipoGalaxia fuera del método
+enum TipoGalaxia { Lenticular, Espiral, Elíptica }
+
+// Definimos la clase Galaxia
+class Galaxia
+{
+    public string Nombre { get; set; }
+    public TipoGalaxia Tipo { get; set; }
+}
