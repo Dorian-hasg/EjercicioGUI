d5b8963c92e5534ec6ae20fcd91cbff16351e430
Dorian-hasg <dorianfernando9994@gmail.com>
2024-10-19 12:06:35 -0400

    Add files via upload

---
+++ ConsoleApp19/ConsoleApp19/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Galaxias cercanas a la Vía Láctea!");
+
+        // Galaxias y sus tipos después de la depuración
+        Console.WriteLine("Cartwheel, Lenticular");
+        Console.WriteLine("Andromeda, Espiral");
+        Console.WriteLine("Waffei 1, Elíptica");
+    }
+}
