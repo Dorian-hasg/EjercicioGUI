c1df4dd8c598db59219d900bb5dd93d1a8dc0e2d
Dorian-hasg <dorianfernando9994@gmail.com>
2024-10-19 12:10:00 -0400

    Add files via upload

---
+++ timer/timer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Temporizador de 10 segundos:");
+
+        // Tiempo en segundos
+        int tiempo = 10;
+
+        // Ciclo para contar regresivamente
+        while (tiempo > 0)
+        {
+            Console.WriteLine($"Tiempo restante: {tiempo} segundos");
+            Thread.Sleep(1000); // Pausa por 1 segundo
+            tiempo--;
+        }
+
+        Console.WriteLine("¡El tiempo ha terminado!");
+    }
+}
